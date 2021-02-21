9ba24bfd572a6c7de6ac7ec4fbf5b7c165b672bf
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-02-21 23:30:10 +0900

    手順８

--- Assets/Sprites/Scripts/EnemyBall.cs
+++ Assets/Sprites/Scripts/EnemyBall.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class EnemyBall : MonoBehaviour
 {
@@ -10,7 +11,27 @@ public class EnemyBall : MonoBehaviour
     {
         capsuleCol = GetComponent<CapsuleCollider2D>();
 
-        
+        ////* ここから追加 *////
+
+        // 最初のスケールを保持
+        Vector2 startScale = transform.localScale;
+
+        // 最小化(見えなくなる)
+
+        transform.localScale = Vector2.zero;
+
+        // Sequence初期化
+        Sequence sequence = DOTween.Sequence();
+
+        // ①敵を回転させながら1.5倍の大きさにし、その後、元の大きさに戻しながら出現させる
+        sequence.Append(transform.DOLocalRotate(new Vector3(0, 720, 0), 1.0f, RotateMode.FastBeyond360).SetEase(Ease.Linear));
+        sequence.Join(transform.DOScale(startScale * 1.5f, 1.0f).SetEase(Ease.InCirc));
+        sequence.AppendInterval(0.15f);
+        sequence.Join(transform.DOScale(startScale, 0.15f).SetEase(Ease.InCirc));
+
+        ////* ここまで追加 *////
+
+
     }
     private void OnCollisionEnter2D(Collision2D col)
     {
@@ -22,6 +43,17 @@ public class EnemyBall : MonoBehaviour
             {
                 // 取得できているか確認
                 Debug.Log(charaBall);
+
+                ////* ここから修正 *////
+
+                // Sequence初期化
+                Sequence sequence = DOTween.Sequence();
+
+                // ②手球と接触すると敵を回転(処理の内容は同じ)
+                transform.DOLocalRotate(new Vector3(0, 720, 0), 0.5f, RotateMode.FastBeyond360).SetEase(Ease.Linear);
+
+                ////* ここまで追加 *////
+
 
                 // TODO 敵の破壊する処理を書く
 
