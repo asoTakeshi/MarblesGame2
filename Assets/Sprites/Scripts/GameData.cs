c0e0ab96934eb65f860ac9ffd58febbe91a97bb0
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-02-22 22:40:22 +0900

    手順１１

---
+++ Assets/Sprites/Scripts/GameData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour
+{
+    public static GameData instance;
+
+    public int charaBallHp;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}   
