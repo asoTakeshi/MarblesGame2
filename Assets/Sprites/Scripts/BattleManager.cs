392c75823d42c47f0d683394cb57c1e96b1e5e6b
DESKTOP-QJ5NU7P\xyz11 <ta.luck69@gmail.com>
2021-02-23 18:29:15 +0900

    手順１３

---
+++ Assets/Sprites/Scripts/BattleManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BattleManager : MonoBehaviour
+{
+    public UIManager uiManager;
+
+    IEnumerator Start()
+    {
+        // 初期化(この処理が終了するまで、次の処理は動かない)
+        yield return StartCoroutine(Initialize());
+    }
+
+    /// <summary>
+    /// ゲーム設定値の初期化
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator Initialize()
+    {
+
+        // 残りの手球の数だけ手球のアイコンの生成する(この処理が終了するまで、次の処理は動かない)
+        yield return StartCoroutine(uiManager.GenerateIconRemainingBalls(GameData.instance.charaBallHp));
+    }
+
+}
---
+++ Assets/Sprites/Scripts/UIManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> iconRemainingBallList = new List<GameObject>();  // 生成した手球アイコンを順番に追加
+
+    [SerializeField]
+    private GameObject iconRemainingBallPrefab;          // アイコンのプレファブ
+
+    [SerializeField]
+    private Transform remainingBallTran;            // アイコンを生成する位置
+
+    /// <summary>
+    /// 手球の残数を画面上に生成
+    /// </summary>
+    /// <param name="ballCount"></param>
+    /// <returns></returns>
+    
+    public IEnumerator GenerateIconRemainingBalls(int ballCount)
+    {
+       // 手球の最大値の数だけfor文を実行して手球アイコンを生成する
+       for(int i = 0; i < ballCount; i++)
+        {
+            // 生成までに少し待つ(順番にアイコンが生成されるようにする演出)
+            yield return new WaitForSeconds(0.15f);
+
+            // 手球アイコンを１つ生成し、icon変数に代入
+            GameObject icon = Instantiate(iconRemainingBallPrefab, remainingBallTran, false);
+
+            // Listに追加
+            iconRemainingBallList.Add(icon);
+        }
+    }
+    /// <summary>
+    /// 手球の残数の表示を更新
+    /// </summary>
+    /// <param name="amount">残りの手球数</param>
+
+    public void UpdateDisplayIocnRemainingBall(int amount)
+    {
+        // Listにある手球アイコンの数だけfor文を実行する
+        for (int i = 0; i < iconRemainingBallList.Count; i++)
+        {
+            if (i < amount)
+            {
+                // 残りの手球数より小さい場合には手球アイコンを表示する
+                iconRemainingBallList[i].SetActive(true);
+            }
+            else
+            {
+                // 大きい場合には手球アイコンを非表示にする
+                iconRemainingBallList[i].SetActive(false);
+            }
+            
+        }
+    }
+
+}
